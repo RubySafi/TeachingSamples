c4ec66a7bacbf4abbed89d03faf6b8bcbdfe21c8
Ruby <onoda319mirai@zpost.plala.or.jp>
2025-07-16 16:39:19 +0900

    01-datetime brief

---
+++ WebSamples/contents/src/index.ts
@@ -0,0 +1,10 @@
+// src/index.ts
+const button = document.getElementById("myButton");
+
+if (button) {
+  button.addEventListener("click", () => {
+    alert("ボタンがクリックされました！！！");
+  });
+} else {
+  console.error("ボタンが見つかりませんでした！！！");
+}
